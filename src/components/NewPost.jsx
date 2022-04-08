4b29db5daaa1d3323cd2713f822501902362ca9a
Christopher Abraham <christopherabraham8@gmail.com>
2022-04-08 14:40:31 +0100

    alomst done

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -3,18 +3,27 @@ import * as faceapi from 'face-api.js';
 
 const NewPost = ({image}) => {
   const {url,width,height} = image;
-  const [faces, setFaces] = useState([])
+  const [faces, setFaces] = useState([]);
+  const [friends, setFriends] = useState([]);
+
   const imgRef = useRef();  
   const canvasRef = useRef(); 
+
   const handleImage = async () => {
-  const detections = await faceapi.detectAllFaces(imgRef.current, new faceapi
-    .TinyFaceDetectorOptions());
-      setFaces(detections.map(d =>Object.values(d.box)));
+    const detections = await faceapi.detectAllFaces(
+      imgRef.current, new faceapi
+    .TinyFaceDetectorOptions()
+    );
+
+      setFaces(detections.map((d) => Object.values(d.box)));
 
   
 };
   const enter = () => {
-    const cxt = canvasref.current.getContext();
+    const ctx = canvasRef.current.getContext('2d');
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = 'yellow';
+    faces.map((face) => ctx.strokeRect(...face));
   }
 
 useEffect(() => {
@@ -29,12 +38,25 @@ useEffect(() => {
   imgRef.current && loadModal();
 }, [])
 
+    const addFriend = (e) => {
+      setFriends(prev=>({...prev,[e.target.name]:e.target.value}));
+    };
+    console.log(friends);
   return (
     <div className='container'>
       <div className="left" style={{width,height }}>
         <img ref={imgRef} crossOrigin='anonymous' src={url} alt="" />
         <canvas onMouseEnter={enter} ref={canvasRef} width={width} height={height}/>
 
+       {faces.map((face,i)=>(
+         <input
+         name={`input${i}`} 
+         style={{left: face[0], top: face[1] + face[3]+ 5}} 
+         placeholder='Tage your friends' key={i} 
+         className='friendsInput'
+         onChange={addFriend}/>
+       ))} 
+      
       </div>
       <div className="right">
       <h1>Share your post</h1>
